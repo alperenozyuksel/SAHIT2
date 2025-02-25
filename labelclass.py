a8eb68d42ec7ac863d5ad48493ecaedad2e6c8c6
mete <ahmkaradeniz@stu.okan.edu.tr>
2025-02-25 15:07:13 +0300

    asd

--- labelclass.py
+++ labelclass.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QFrame, QLabel
 from PyQt5.QtCore import Qt
 
 class LabelClass(QLabel):
-    def __init__(self, text, parent, color, bg_color, font_weight, font_size):
+    def __init__(self, text, parent, color, bg_color, font_weight, font_size, width=None, height=None):
         super().__init__(text, parent)
 
         self.color = color
@@ -12,3 +12,6 @@ class LabelClass(QLabel):
 
         self.setStyleSheet(f"color: {self.color}; background-color: {self.bg_color}; font-weight: {self.font_weight}; font-size: {self.font_size}px;")
         self.setAlignment(Qt.AlignCenter)
+
+        if width and height:
+            self.setFixedSize(width, height)
